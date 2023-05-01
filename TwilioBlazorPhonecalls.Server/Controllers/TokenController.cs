4e78aff85a29fd03c64f458935e38447e73a22d1
Fábio Sander Novaes Corrêa <fabio.correa@linedata.com.br>
2023-05-01 12:09:48 -0300

    first commit

---
+++ TwilioBlazorPhonecalls.Server/Controllers/TokenController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Twilio.Jwt.AccessToken;
+using Microsoft.AspNetCore.Cors;
+
+namespace TwilioBlazorPhonecalls.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        //private static readonly HttpClient httpClient = new HttpClient();
+
+        public TokenController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        [EnableCors("BlazorClientPolicy")]
+        public string Get()
+        {
+            string twilioAccountSid = configuration["TwilioAccountSid"];
+            string twilioApiKey = configuration["TwilioApiKey"];
+            string twilioApiSecret = configuration["TwilioApiSecret"];
+            string twiMLApplicationSid = configuration["TwiMLApplicationSid"];
+
+            var grants = new HashSet<IGrant>();
+            // Create a Voice grant for this token
+            grants.Add(new VoiceGrant
+            {
+                OutgoingApplicationSid = twiMLApplicationSid,
+                IncomingAllow = true
+            });
+
+            // Create an Access Token generator
+            var token = new Token(
+                twilioAccountSid,
+                twilioApiKey,
+                twilioApiSecret,
+                // identity will be used as the client name for incoming dials
+                identity: "blazor_client",
+                grants: grants
+            );
+
+            return token.ToJwt();
+        }
+    }
+}
---
+++ TwilioBlazorPhonecalls.Server/Controllers/VoiceController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+
+namespace TwilioBlazorPhonecalls.Server.Controllers
+{
+    [ApiController]
+    public class VoiceController : TwilioController
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<VoiceController> logger;
+
+        public VoiceController(IConfiguration configuration, ILogger<VoiceController> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        [Route("voice/outgoing")]
+        public TwiMLResult Outgoing([FromForm] string to)
+        {
+            string twilioPhoneNumber = configuration["TwilioPhoneNumber"];
+            var response = new VoiceResponse();
+            var dial = new Dial();
+            logger.LogInformation($"Calling {to}");
+            dial.CallerId = twilioPhoneNumber;
+            dial.Number(to);
+            response.Append(dial);
+            return TwiML(response);
+        }
+    }
+}
